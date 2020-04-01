8185aea12288dd647d5f985ccce51025f4df7f23
perillaroc <perillaroc@gmail.com>
2020-04-01 12:19:36 +0800

    :art: use argparse module to implement sub options parser.

--- nwpc_data/data_finder/__main__.py
+++ nwpc_data/data_finder/__main__.py
@@ -1,3 +1,5 @@
+import argparse
+
 import click
 import pandas as pd
 
@@ -47,13 +49,11 @@ def find_local(data_type, data_level, config_dir, query_args):
 
 
 def find_oper_file(config: dict, data_level: str, query_args: tuple):
-    parser = click.OptionParser()
-    parser.add_option(["--start-time"], dest='start_time')
-    parser.add_option(["--forecast-time"], dest='forecast_time')
-    (values, args, order) = parser.parse_args(list(query_args))
+    parser = create_oper_option_parser()
+    args = parser.parse_args(query_args)
 
-    forecast_time = pd.to_timedelta(values["forecast_time"])
-    start_time = pd.to_datetime(values["start_time"], format="%Y%m%d%H")
+    forecast_time = pd.to_timedelta(args.forecast_time)
+    start_time = pd.to_datetime(args.start_time, format="%Y%m%d%H")
 
     file_path = find_file(config, data_level, start_time, forecast_time)
     if file_path is None:
@@ -63,20 +63,37 @@ def find_oper_file(config: dict, data_level: str, query_args: tuple):
 
 
 def find_eps_file(config: dict, data_level: str, query_args: tuple):
-    parser = click.OptionParser()
-    parser.add_option(["--start-time"], dest='start_time')
-    parser.add_option(["--forecast-time"], dest='forecast_time')
-    parser.add_option(["--number"], dest='number')
-    (values, args, order) = parser.parse_args(list(query_args))
+    parser = create_eps_option_parser()
+    args = parser.parse_args(query_args)
 
-    forecast_time = pd.to_timedelta(values["forecast_time"])
-    start_time = pd.to_datetime(values["start_time"], format="%Y%m%d%H")
+    forecast_time = pd.to_timedelta(args.forecast_time)
+    start_time = pd.to_datetime(args.start_time, format="%Y%m%d%H")
 
-    file_path = find_file(config, data_level, start_time, forecast_time, number=int(values["number"]))
+    file_path = find_file(config, data_level, start_time, forecast_time, number=args.number)
     if file_path is None:
         print("None")
     else:
         print(file_path)
+
+
+def create_oper_option_parser():
+    parser = argparse.ArgumentParser(description='Parse options for stream oper.')
+    parser.add_argument('--start-time', dest="start_time",
+                        help='start time, such as YYYMMDDHH')
+    parser.add_argument('--forecast-time', dest='forecast_time',
+                        help='forecast time, such as 3h')
+    return parser
+
+
+def create_eps_option_parser():
+    parser = argparse.ArgumentParser(description='Parse options for stream oper.')
+    parser.add_argument('--start-time', dest="start_time",
+                        help='start time, such as YYYMMDDHH')
+    parser.add_argument('--forecast-time', dest='forecast_time',
+                        help='forecast time, such as 3h')
+    parser.add_argument('--number', dest='number', type=int,
+                        help='member number')
+    return parser
 
 
 if __name__ == "__main__":
