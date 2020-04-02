f3e31e3527d6abc79847d6a4b3d0ad44fdbf5053
perillaroc <perillaroc@gmail.com>
2020-04-02 20:21:35 +0800

    :truck: add load_bytes_from_file into eccodes module.

--- nwpc_data/grib/eccodes/__init__.py
+++ nwpc_data/grib/eccodes/__init__.py
@@ -2,6 +2,11 @@ from ._message import (
     load_message_from_file,
     load_messages_from_file,
 )
+
 from ._xarray import (
     load_field_from_file,
+)
+
+from ._bytes import (
+    load_bytes_from_file
 )
