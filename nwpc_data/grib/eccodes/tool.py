0e14f2961d4e6f0de8a82a2b76e9a1d744b64369
perillaroc <perillaroc@gmail.com>
2020-04-02 16:06:51 +0800

    :art: fix level type for load_bytes_from_file.

--- nwpc_data/grib/eccodes/tool.py
+++ nwpc_data/grib/eccodes/tool.py
@@ -5,35 +5,40 @@ import io
 import eccodes
 
 from nwpc_data.grib.eccodes._util import (
-    _check_parameter,
-    _check_level_type,
-    _check_level_value,
+    _check_message,
 )
+from nwpc_data.grib._level import fix_level_type
 
 
 def load_bytes_from_file(
         file_path: str or Path,
         parameter: str or typing.Dict,
-        level_type: str,
-        level: int,
-        **kwargs,
-) -> int or None:
+        level_type: str = None,
+        level: int = None,
+) -> io.BytesIO or None:
+    """
+    Load one message from grib file and return a file-like io.BytesIO object.
+
+    Parameters
+    ----------
+    file_path
+    parameter
+    level_type
+    level
+
+    Returns
+    -------
+    io.BytesIO or None
+    """
     offset = 0
+    fixed_level_type = fix_level_type(level_type)
     with open(file_path, "rb") as f:
         while True:
             message_id = eccodes.codes_grib_new_from_file(f)
             length = eccodes.codes_get(message_id, "totalLength")
             if message_id is None:
                 return None
-            if not _check_parameter(message_id, parameter):
-                eccodes.codes_release(message_id)
-                offset += length
-                continue
-            if not _check_level_type(message_id, level_type):
-                eccodes.codes_release(message_id)
-                offset += length
-                continue
-            if not _check_level_value(message_id, level):
+            if not _check_message(message_id, parameter, fixed_level_type, level):
                 eccodes.codes_release(message_id)
                 offset += length
                 continue
