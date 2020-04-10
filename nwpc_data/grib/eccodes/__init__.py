0acb1dd380ed23987d3a3edca2453a2dd4ac2b09
perillaroc <perillaroc@gmail.com>
2020-04-10 14:56:23 +0800

    :art: move load_filed_from_file into _field.py

--- nwpc_data/grib/eccodes/__init__.py
+++ nwpc_data/grib/eccodes/__init__.py
@@ -3,9 +3,7 @@ from ._message import (
     load_messages_from_file,
 )
 
-from ._xarray import (
-    load_field_from_file,
-)
+from ._field import load_field_from_file
 
 from ._bytes import (
     load_bytes_from_file,
