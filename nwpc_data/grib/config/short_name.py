8ffac0e743038c8d4e15f3f48735eb742e51be56
perillaroc <perillaroc@gmail.com>
2021-05-14 09:58:19 +0800

    :sparkles: update short name table from GRAPES GEPS.

--- nwpc_data/grib/config/short_name.py
+++ nwpc_data/grib/config/short_name.py
@@ -1,10 +1,18 @@
+"""
+Short name table from WGRIB2 project.
+
+Collect these short names from the following systems:
+
+- GRAPES GFS
+- GRAPES MESO
+"""
+
 import io
 
 import pandas as pd
 
 
-_SHORT_NAME_TABLE_CONTENT = """
-short_name,discipline,parameterCategory,parameterNumber
+_SHORT_NAME_TABLE_CONTENT = """short_name,discipline,parameterCategory,parameterNumber
 ACPCP,0,1,10
 ALBDO,0,19,1
 APCP,0,1,8
@@ -14,6 +22,8 @@ CAPE,0,7,6
 CDCC,0,6,22
 CIN,0,7,7
 CLWMR,0,1,22
+CR,0,16,224
+DCAPE,0,7,224
 DEPR,0,0,7
 DPT,0,0,6
 DZDT,0,2,9
@@ -23,6 +33,7 @@ GUST,0,2,22
 HCDC,0,6,5
 HFLUX,2,0,24
 HGT,0,3,5
+HI,0,1,239
 HLCY,0,7,8
 HPBL,0,3,18
 ICMR,0,1,23
@@ -46,8 +57,10 @@ RH,0,1,1
 RI,0,7,12
 RWMR,0,1,24
 SHWINX,0,7,13
+SKINT,0,0,17
 SNMR,0,1,25
 SNOD,0,1,11
+SOIL_M,2,0,22
 SPFH,0,1,0
 SX,0,7,5
 TCDC,0,6,1
@@ -58,6 +71,8 @@ TMAX,0,0,4
 TMIN,0,0,5
 TMP,0,0,0
 TOTALX,0,7,4
+TSOIL,2,0,2
+TSRWE,0,1,53
 UGRD,0,2,2
 ULWRF,0,5,4
 UPHL,0,7,15
