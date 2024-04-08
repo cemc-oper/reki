a69287d2d1a3099f2fc7378c1722b02790e6dba0
perillaroc <perillaroc@gmail.com>
2024-04-08 16:26:15 +0800

    :package: use setuptools_scm to generate version dynamically.

--- reki/__init__.py
+++ reki/__init__.py
@@ -1,1 +1,7 @@
-__version__ = "2023.05.1"
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("package-name")
+except PackageNotFoundError:
+    # package is not installed
+    pass
